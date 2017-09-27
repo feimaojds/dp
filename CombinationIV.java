fc0027803240946ed616eb2ce4f1f600abcf85d8
feimaojds <313061793@qq.com>
2017-09-27 21:22:57 +0800

    Given an integer array with all positive numbers and no duplicates, find the number of possible combinations that add up to a positive integer target.
    Example:
    
    nums = [1, 2, 3]
    target = 4
    
    The possible combination ways are:
    (1, 1, 1, 1)
    (1, 1, 2)
    (1, 2, 1)
    (1, 3)
    (2, 1, 1)
    (2, 2)
    (3, 1)
    
    Note that different sequences are counted as different combinations.
    
    Therefore the output is 7.
    
    思路：可用回溯法，但是超时。
            动态规划。
            dp[i]表示  相加和为i，有几种方法。
           外围for，自底向上，表示元素可以重复迭代，1可用多次，2可用多次。……
                          内围for，表示每次所有元素都可使用。

---
+++ CombinationIV.java
@@ -0,0 +1,24 @@
+package leecode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationIV {
+
+	
+public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target+1];
+        dp[0] =1;
+        for(int i=1;i<dp.length;i++){
+        	for(int j=0;j<nums.length;j++){
+        		if(i>=nums[j]){
+        			dp[i] = dp[i]+dp[i-nums[j]];
+        		}
+        	}
+        }
+        return dp[target];
+    }
+
+
+
+}
