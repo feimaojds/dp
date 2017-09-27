c29108420b1731b7716f0214812071c713d4bf91
feimaojds <313061793@qq.com>
2017-09-27 16:49:23 +0800

    Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them as an array.
    Example:
    For num = 5 you should return [0,1,1,2,1,2].
    
    思路： 
                 动态规划。res[i]表示i这个整数的1的个数。  res[i] = 将其右移一位后数字的res  +  右移的那位数字是否为1   ；不用每个res都32位计算。

---
+++ CountingBits.java
@@ -0,0 +1,13 @@
+package leecode;
+
+public class CountingBits {
+	
+	 public int[] countBits(int num) {
+	        int[] res = new int[num+1];
+	        for(int i=1;i<res.length;i++){
+	        	res[i] = res[i>>1] + (i&1);
+	        }
+	        return res;
+	    }
+
+}
